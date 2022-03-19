00fca6d281f9624283aa0506d4d70f3d46550571
Daniel Voogsgerd <daniel@voogsgerd.nl>
2022-03-19 12:33:19 +0100

    Update API

--- intervals_api.py
+++ intervals_api.py
@@ -1,9 +1,12 @@
+from abc import abstractclassmethod, abstractmethod
 import requests
 import pandas as pd
 
 from io import StringIO
 import datetime
-from typing import Tuple, Set
+from typing import Tuple, Set, Dict, Any
+
+from abc import ABC
 
 import logging
 
@@ -11,47 +14,16 @@ import re
 
 API_URL = "https://intervals.icu"
 
-class API:
-    """A wrapper for all the Endpoints in the API."""
 
-    def __init__(self, athlete_id, api_key):
-        assert self.validate_athlete_id(athlete_id)
-        assert self.validate_api_key(api_key)
-
-        self.athlete_id = athlete_id
-        self.api_key = api_key
-
+class Endpoint(ABC):
+    @abstractmethod
     @property
-    def events(self):
-        return EventsEndpoint(self.athlete_id, self.api_key)
+    def endpoint_url_format(self) -> str:
+        pass
 
-    @property
-    def wellness(self):
-        return WellnessEndpoint(self.athlete_id, self.api_key)
+    def url(self, url_format="", **kwargs) -> str:
+        return (API_URL + self.endpoint_url_format + url_format).format(athlete_id=self.athlete_id, **kwargs)
 
-    @property
-    def calendar(self):
-        return CalendarEndpoint(self.athlete_id, self.api_key)
-
-    @property
-    def wellness_csv(self):
-        return WellnessCSVEndpoint(self.athlete_id, self.api_key)
-
-    @property
-    def activities_csv(self):
-        return ActivitiesCSVEndpoint(self.athlete_id, self.api_key)
-
-    @staticmethod
-    def validate_api_key(api_key: str) -> bool:
-        return bool(re.match(r'\w{24}', api_key))
-
-    @staticmethod
-    def validate_athlete_id(athlete_id: str) -> bool:
-        return bool(re.match(r'i\d+', athlete_id))
-
-
-class Endpoint:
-    endpoint_url: str
     auth: Tuple[str, str]
     athlete_id: str
 
@@ -60,10 +32,10 @@ class Endpoint:
         self.auth = ("API_KEY", api_key)
         self.athlete_id = athlete_id
 
-    def get_request(self, url, query_parameters=None):
+    def get_request(self, url_format="", query_parameters=None) -> Dict[Any, Any]:
         """Perform JSON requests on a certain endpoint."""
         r = requests.get(
-            url.format(athlete_id=self.athlete_id),
+            self.url(url_format),
             auth=self.auth,
             params=query_parameters,
         )
@@ -71,10 +43,11 @@ class Endpoint:
         logging.debug(f"Sending GET request to {r.url}")
         return r.json()
 
-    def get_content_request(self, url, query_parameters=None):
+    def get_content_request(self, url_format, query_parameters=None):
         """Perform requests on a certain endpoint as text."""
+        complete_url_format = self.endpoint_url_format + url_format
         r = requests.get(
-            url.format(athlete_id=self.athlete_id),
+            self.url(url_format),
             auth=self.auth,
             params=query_parameters,
         )
@@ -82,10 +55,10 @@ class Endpoint:
         logging.debug(f"Sending GET request to {r.url}")
         return r.text
 
-    def put_request(self, url, data, query_parameters=None):
+    def put_request(self, url_format, data, query_parameters=None):
         """Perform a put request on a certain endpoint."""
         r = requests.put(
-            url.format(athlete_id=self.athlete_id),
+            self.url(url_format),
             auth=self.auth,
             params=query_parameters,
             json=data
@@ -94,13 +67,12 @@ class Endpoint:
         logging.debug(f"Sending PUT request to {r.url}")
         return r.json()
 
-    def delete_request(self, url, query_parameters=None):
+    def delete_request(self, url_format, query_parameters=None):
         """Perform a put request on a certain endpoint."""
         r = requests.put(
-            url.format(athlete_id=self.athlete_id),
+            self.url(url_format),
             auth=self.auth,
-            params=query_parameters,
-            json=data
+            params=query_parameters
         )
 
         logging.debug(f"Sending DELETE request to {r.url}")
@@ -108,9 +80,9 @@ class Endpoint:
 
 
 class CSVEndpoint(Endpoint):
-    def get_request_csv(self, url, query_parameters=None):
+    def get_request_csv(self, url_format, query_parameters=None) -> pd.DataFrame:
         r = requests.get(
-            url.format(athlete_id=self.athlete_id) + ".csv",
+            self.url(url_format + ".csv"),
             auth=self.auth,
             params=query_parameters,
         )
@@ -121,11 +93,11 @@ class CSVEndpoint(Endpoint):
         response_file = StringIO(response_content)
         return pd.read_csv(response_file)
 
-    def post_request_csv(self, url, data: pd.DataFrame, index_label):
+    def post_request_csv(self, url_format, data: pd.DataFrame, index_label):
         csv = data.to_csv(index_label=index_label)
 
         r = requests.post(
-            url.format(athlete_id=self.athlete_id),
+            self.url(url_format),
             auth=self.auth,
             files={
                 'file': ('wellness.csv', csv)
@@ -138,15 +110,15 @@ class CSVEndpoint(Endpoint):
 
 
 class CalendarEndpoint(Endpoint):
-    endpoint_url = API_URL + "/api/v1/athlete/{athlete_id}/calendars"
+    endpoint_url_format = "/api/v1/athlete/{athlete_id}/calendars"
 
     def get(self):
         """Retreive a list of all calendars."""
-        return self.get_request(self.endpoint_url)
+        return self.get_request()
 
 
 class EventsEndpoint(Endpoint):
-    endpoint_url = API_URL + "/api/v1/athlete/{athlete_id}/events"
+    endpoint_url_format = "/api/v1/athlete/{athlete_id}/events"
 
     def list(self, oldest: datetime.date, newest: datetime.date):
         """List all the events between two dates."""
@@ -158,7 +130,7 @@ class EventsEndpoint(Endpoint):
         params["oldest"] = oldest.isoformat()
         params["newest"] = newest.isoformat()
 
-        return self.get_request(self.endpoint_url, query_parameters=params)
+        return self.get_request(query_parameters=params)
 
     def create(self, event_data):
         raise NotImplementedError
@@ -168,7 +140,7 @@ class EventsEndpoint(Endpoint):
 
     def delete(self, event_id: int, event_data):
         """Untested."""
-        return self.delete_request(self.endpoint_url + "/" + str(event_id))
+        return self.delete_request(f"/{event_id}")
 
     def download(self, event_id, workout_file_type):
         raise NotImplementedError
@@ -185,27 +157,25 @@ class ActivitiesCSVEndpoint(CSVEndpoint):
     will require more requests when retrieving multiple activities.
     """
 
-    endpoint_url = API_URL + "/api/v1/athlete/{athlete_id}/activities"
+    endpoint_url_format = "/api/v1/athlete/{athlete_id}/activities"
 
     def get(self):
         """Retrieve multiple activities."""
-        return self.get_request_csv(
-            self.endpoint_url.format(athlete_id=self.athlete_id)
-        )
+        return self.get_request_csv()
 
 
 class WellnessEndpoint(Endpoint):
-    endpoint_url = API_URL + "/api/v1/athlete/{athlete_id}/wellness"
+    endpoint_url_format = "/api/v1/athlete/{athlete_id}/wellness"
 
     def get(self, date: datetime.date):
         """Get wellness info for a certain date."""
         assert isinstance(date, datetime.date)
-        return self.get_request(self.endpoint_url + "/" + date.isoformat())
+        return self.get_request(f"/{date.isoformat()}")
 
     def update(self, date: datetime.date, data):
         """Update wellness info for a certain date."""
         assert isinstance(date, datetime.date)
-        return self.put_request(self.endpoint_url + "/" + date.isoformat(), data)
+        return self.put_request(f"/{date.isoformat()}", data)
 
 
 class WellnessCSVEndpoint(CSVEndpoint):
@@ -216,7 +186,7 @@ class WellnessCSVEndpoint(CSVEndpoint):
     records, but this one will use less requests when updating a lot of data.
     """
 
-    endpoint_url = API_URL + "/api/v1/athlete/{athlete_id}/wellness"
+    endpoint_url_format = "/api/v1/athlete/{athlete_id}/wellness"
 
     def __init__(self, athlete, auth_key):
         super().__init__(athlete, auth_key)
@@ -242,14 +212,55 @@ class WellnessCSVEndpoint(CSVEndpoint):
             params["cols"] = ",".join(cols)
 
         return self.get_request_csv(
-            self.endpoint_url.format(**kwargs, athlete_id=self.athlete_id),
+            # self.endpoint_url.format(**kwargs, athlete_id=self.athlete_id),
+            self.url(**kwargs),
             query_parameters=params,
         )
 
     def update(self, data: pd.DataFrame, index_label, **kwargs):
         """Update multiple wellness entries"""
         return self.post_request_csv(
-            self.endpoint_url.format(**kwargs, athlete_id=self.athlete_id),
+            # self.endpoint_url.format(**kwargs, athlete_id=self.athlete_id),
+            self.url(**kwargs),
             data=data,
             index_label=index_label
         )
+
+
+class API:
+    """A wrapper for all the Endpoints in the API."""
+
+    def __init__(self, athlete_id, api_key):
+        assert self.validate_athlete_id(athlete_id)
+        assert self.validate_api_key(api_key)
+
+        self.athlete_id = athlete_id
+        self.api_key = api_key
+
+    @property
+    def events(self) -> EventsEndpoint:
+        return EventsEndpoint(self.athlete_id, self.api_key)
+
+    @property
+    def wellness(self) -> WellnessEndpoint:
+        return WellnessEndpoint(self.athlete_id, self.api_key)
+
+    @property
+    def calendar(self) -> CalendarEndpoint:
+        return CalendarEndpoint(self.athlete_id, self.api_key)
+
+    @property
+    def wellness_csv(self) -> WellnessCSVEndpoint:
+        return WellnessCSVEndpoint(self.athlete_id, self.api_key)
+
+    @property
+    def activities_csv(self) -> ActivitiesCSVEndpoint:
+        return ActivitiesCSVEndpoint(self.athlete_id, self.api_key)
+
+    @staticmethod
+    def validate_api_key(api_key: str) -> bool:
+        return bool(re.match(r'\w{24}', api_key))
+
+    @staticmethod
+    def validate_athlete_id(athlete_id: str) -> bool:
+        return bool(re.match(r'i\d+', athlete_id))
